c0e6cde43e3114f4af565d2f38333e4c626c8dd3
Enric Cervera <ecervera@uji.es>
2020-04-26 10:46:44 +0200

    add kobuki and UR

--- start_kinetic.bash
+++ start_kinetic.bash
@@ -1,7 +1,12 @@
 #!/bin/bash
-xhost +
-docker run --gpus=all --rm -p 8888:8888 \
-           --env="DISPLAY" \
-           --env="QT_X11_NO_MITSHM=1" \
-           -v /tmp/.X11-unix:/tmp/.X11-unix gazebolab:kinetic
-xhost -
+xhost +local:root
+
+docker run -it \
+    --gpus=all \
+    --env="DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    gazebolab:kinetic
+    /bin/bash
+
+xhost -local:root
